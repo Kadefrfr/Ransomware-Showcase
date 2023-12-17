83e2b4d128b94080ad4995d85fa8eace2ca45225
Kadefrfr <153398756+Kadefrfr@users.noreply.github.com>
2023-12-16 19:46:52 -0600

    DE.py

---
+++ DE.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+
+files = []
+
+for file in os.listdir()
+	if file == "EN.py", or file == "thekey.key", or file == "DE.py:
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+
+with open("thekey.key", "rb") as key:
+        secretkey = key.read()
+
+
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_decrypted = Fernet(secretkey).decrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
